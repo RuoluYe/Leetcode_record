45a23903c2a0329769eb2e8e714c747df0816a23
RuoluYe <yeruolu@uw.edu>
2023-06-29 11:22:04 -0700

    dynamic

--- 322.coin-change.py
+++ 322.coin-change.py
@@ -24,17 +24,17 @@ class Solution:
 # identify base case, condition, choice
 
 
-#自顶向下，暴力
-# class Solution:
-#     def coinChange(self, coins: List[int], amount: int) -> int:
-#         if amount == 0:
-#             return 0
-#         if amount < 0:
-#             return -1
-#         res = float('inf')
-#         for coin in coins:
-#             sub = self.coinChange(coins, amount - coin)
-#             if sub == -1:
-#                 continue
-#             res = min(res, sub+1) # 1 + min(coinChange(coins, amount - coin)) = coins amount
-#         return res if res!=float('inf') else -1
+# 自顶向下，暴力
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount == 0:
+            return 0
+        if amount < 0:
+            return -1
+        res = float('inf')
+        for coin in coins:
+            sub = self.coinChange(coins, amount - coin)
+            if sub == -1:
+                continue
+            res = min(res, sub+1) # 1 + min(coinChange(coins, amount - coin)) = coins amount
+        return res if res!=float('inf') else -1
