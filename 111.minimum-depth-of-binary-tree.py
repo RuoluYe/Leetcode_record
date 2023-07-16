40329f272e289d25a24e3ceddfb84b8e5e762f98
RuoluYe <yeruolu@uw.edu>
2023-07-15 21:25:59 -0700

    dfs solution

--- 111.minimum-depth-of-binary-tree.py
+++ 111.minimum-depth-of-binary-tree.py
@@ -16,25 +16,28 @@ class Solution:
         
         if not root:
             return 0
-        count = 1
-        from queue import Queue
-        q = Queue()
-        q.put(root)
+        # count = 1
+        # from queue import Queue
+        # q = Queue()
+        # q.put(root)
         
-        while q:
-            s = q.qsize()
-            for i in range(s):
-                cur = q.get()
-                if not cur.left and not cur.right:
-                    return count
+        # while q:
+        #     s = q.qsize()
+        #     for i in range(s):
+        #         cur = q.get()
+        #         if not cur.left and not cur.right:
+        #             return count
                 
-                if cur.left:
-                    q.put(cur.left)
-                if cur.right:
-                    q.put(cur.right)
-            count += 1 # update depth outside after examine all node at this level(after for loop ends)
+        #         if cur.left:
+        #             q.put(cur.left)
+        #         if cur.right:
+        #             q.put(cur.right)
+        #     count += 1 # update depth outside after examine all node at this level(after for loop ends)
         
-        return count
+        # return count
+        l = self.minDepth(root.left)
+        r = self.minDepth(root.right)
+        return l+r+1 if l == 0 or r == 0 else min(l,r)+1
         
 # @lc code=end
 
