3ec78e669547b7bf89cd0d437108509b0fc51645
RuoluYe <yeruolu@uw.edu>
2023-07-16 23:51:33 -0700

    bfs tree

--- 111.minimum-depth-of-binary-tree.py
+++ 111.minimum-depth-of-binary-tree.py
@@ -11,33 +11,41 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# BFS
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
-        
         if not root:
             return 0
-        # count = 1
-        # from queue import Queue
-        # q = Queue()
-        # q.put(root)
         
-        # while q:
-        #     s = q.qsize()
-        #     for i in range(s):
-        #         cur = q.get()
-        #         if not cur.left and not cur.right:
-        #             return count
+        from queue import Queue
+        q = Queue()
+        q.put(root)
+        depth = 1
+        
+        
+        while q:
+            s = q.qsize()
+            for i in range(s):
+                cur = q.get()
+                if not cur.left and not cur.right:
+                    return depth
                 
-        #         if cur.left:
-        #             q.put(cur.left)
-        #         if cur.right:
-        #             q.put(cur.right)
-        #     count += 1 # update depth outside after examine all node at this level(after for loop ends)
+                if cur.left:
+                    q.put(cur.left)
+                if cur.right:
+                    q.put(cur.right)
+            depth += 1 # update depth outside after examine all node at this level(after for loop ends)
         
-        # return count
-        l = self.minDepth(root.left)
-        r = self.minDepth(root.right)
-        return l+r+1 if l == 0 or r == 0 else min(l,r)+1
+        return depth
         
 # @lc code=end
 
+#DFS
+def minDepth(self, root: Optional[TreeNode]) -> int:
+    if not root:
+        return 0
+    l = self.minDepth(root.left)
+    r = self.minDepth(root.right)
+    return l+r+1 if l == 0 or r == 0 else min(l,r)+1
+        
