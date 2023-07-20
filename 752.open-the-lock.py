d5b4e87541cc6f204a93a1653ff7be667242236e
RuoluYe <yeruolu@uw.edu>
2023-07-19 17:46:34 -0700

    bfs  frame

---
+++ 752.open-the-lock.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=752 lang=python3
+#
+# [752] Open the Lock
+#
+
+# @lc code=start
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        
+# @lc code=end
+
