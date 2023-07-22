92c9c86a60d99110f545ccfd725a9615d7900b65
RuoluYe <yeruolu@uw.edu>
2023-07-22 02:23:40 -0700

    slow fast pointer

--- 202.happy-number.py
+++ 202.happy-number.py
@@ -7,19 +7,17 @@
 # @lc code=start
 class Solution:
     def isHappy(self, n: int) -> bool:
-        seen = set()
         def getSum(n):
             res = 0
             while n > 0:
                 res += (n%10)**2
                 n = n//10
             return res
-        while n not in seen:
-            seen.add(n)
-            n = getSum(n)
-            if n == 1:
-                return True
-        return False
+        slow, fast = getSum(n) , getSum(getSum(n))
+        while(slow!=fast):
+            slow = getSum(slow)
+            fast = getSum(getSum(fast))
+        return slow == 1
             
 # @lc code=end
 
