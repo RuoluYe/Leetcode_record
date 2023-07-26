7ec1d3a9caa3b213a3b580acb04685166c3edf82
RuoluYe <yeruolu@uw.edu>
2023-07-25 22:59:26 -0700

    sliding window

--- 567.permutation-in-string.py
+++ 567.permutation-in-string.py
@@ -7,6 +7,29 @@
 # @lc code=start
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s2) < len(s1):
+            return False
+        need,window = {}, {}
+        for i in s1:
+            need[i] = need.get(i,0)+1
+        left,right = 0,0
+        while right < len(s2):
+            ch = s2[right]
+            right += 1
+            if ch in need:
+                window[ch] = window.get(ch,0)+1
+            while right - left >= len(s1):
+                if window == need:
+                    return True
+                d = s2[left]
+                left +=1
+                if d in need:
+                    window[d]-=1
+                    if window[d] == 0:
+                        window.pop(d)
+        return False
+                
+                    
         
 # @lc code=end
 
