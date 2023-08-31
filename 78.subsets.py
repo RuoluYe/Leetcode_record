7fcbe34ca80e90e46d75227e8a1be22e4990eba2
RuoluYe <yeruolu@uw.edu>
2023-08-30 17:52:26 -0700

    backtracking combination

--- 78.subsets.py
+++ 78.subsets.py
@@ -6,23 +6,17 @@
 
 # @lc code=start
 class Solution:
-    def subsets(self, nums):
-        res = [[]]
-        for n in nums:
-            for i in range(len(res)):
-                res.append(res[i] + [n])
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        track = []
+        def backtrack(nums, start):
+            res.append(list(track))
+            for i in range(start, len(nums)):
+                track.append(nums[i])
+                backtrack(nums, i+1)
+                track.pop()
+        backtrack(nums,0)
         return res
-    # def subsets(self, nums: List[int]) -> List[List[int]]:
-    #     res = []
-    #     track = []
-    #     def backtrack(nums, start):
-    #         res.append(list(track))
-    #         for i in range(start, len(nums)):
-    #             track.append(nums[i])
-    #             backtrack(nums, i+1)
-    #             track.pop()
-    #     backtrack(nums,0)
-    #     return res
 # @lc code=end
 
 def subsets(self, nums):
