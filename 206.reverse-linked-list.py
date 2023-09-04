f0876583b5e54c36016ee166f50a6c7c111a2729
RuoluYe <yeruolu@uw.edu>
2023-09-04 15:16:17 -0700

    backtracking+reverse linked list

---
+++ 206.reverse-linked-list.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=206 lang=python3
+#
+# [206] Reverse Linked List
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if (head == None or head.next == None):
+            return head
+        last = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return last
+# @lc code=end
+
